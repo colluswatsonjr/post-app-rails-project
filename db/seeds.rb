fde4f94c54e5fce0b7bf5abb09def469a15c027d
Collus Watson <cklwatsonjr@gmail.com>
2022-11-26 11:23:09 -0500

    'create user table'

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(username: 'hearLouis', password_digest: 'asdf')
+User.create(username: 'seeTre', password_digest: 'asdf')
+User.create(username: 'speakCollus', password_digest: 'asdf')
